af2517ae0dc83d7c6c9d8dd6c12cd84cb32c3c0e
f@b Desktop <fab327@hotmail.com>
2015-04-26 15:05:33 -0400

    Another update to db string for heroku

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 //mongoose.connect('mongodb://localhost/vault');
-mongoose.connect(' mongodb://Onlinevault:onl1nevault@ds063809.mongolab.com:63809/heroku_app36265103');
+mongoose.connect('mongodb://marcelstaro@gmail.com:n2KwTdSq6DkGM89w@ds063809.mongolab.com:63809/heroku_app36265103');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
